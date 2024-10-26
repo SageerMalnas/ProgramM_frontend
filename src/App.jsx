0994f80300c9568bce38ee03db42dc906ac4c473
SageerMalnas <sageermalnas78@gmail.com>
2024-10-27 00:01:28 +0530

    added protected routes for auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { AuthProvider } from './context/AuthContext'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Register from './pages/Auth/Register';
@@ -10,6 +10,7 @@ import Analytics from './pages/Admin/Analytics/Analytics';
 import Settings from './pages/Admin/Settings/Settings';
 import Board from './pages/Admin/Board/Board';
 import { TaskProvider } from './context/TaskContext';
+import ProtectedRoute from './protectedRoute';
 
 function App() {
 
@@ -18,13 +19,18 @@ function App() {
     <AuthProvider>
       <TaskProvider>
       <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
         <Route path='/register' element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute />}>
         <Route path='/dashboard' element={<Dashboard />}>
+        <Route index element={<Navigate to="board" />} />
           <Route path='analytics' element={<Analytics />} />
           <Route path='settings' element={<Settings />} />
           <Route path='board' element={<Board/>}/>
         </Route>
+        </Route>
+        <Route path='*' element={<Navigate to="/login"/>}/>
       </Routes>
       </TaskProvider>
       <ToastContainer />
